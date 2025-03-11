a04b528552bfc3813794e613c2a6e5fe0aae8f0b
LeonardoBrito-XLBR <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2025-03-11 15:45:56 -0300

    Add files via upload

---
+++ aula06/bank/Gerente.java
@@ -0,0 +1,15 @@
+package bank;
+
+class Gerente extends Funcionario {
+    private String senha;
+    private int quantidadeFuncionario;
+
+    public Gerente(){
+        // super(nome, , getSalario())
+    }
+    
+    public String getSenha(){
+        return senha;
+    }
+
+}
